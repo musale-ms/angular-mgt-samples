dfe6c3b3e9295e8c89b9caa409376a671a83c49d
Musale Martin <martinmusale@microsoft.com>
2023-07-18 19:03:29 +0300

    Add app component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Providers, Msal2Provider, TemplateHelper } from '@microsoft/mgt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'test-mgt-angular';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    Providers.globalProvider = new Msal2Provider({
+      clientId: 'YOUR CLIENT ID',
+    });
+    TemplateHelper.setBindingSyntax('[[', ']]');
+  }
 }
